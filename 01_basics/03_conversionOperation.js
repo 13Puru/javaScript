a6008408d76ec89586d9a04cee4b1bbfda9ba9e0
Purab <13purab@gmail.com>
2025-01-19 12:54:41 +0000

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,49 @@
+let score = "33abc"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" - 33
+// "33abc" - NaN
+// true - 1; flase - 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+//1 - true;  0 - false
+//"" - flase; "purab" - true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ***************Operations********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2**3); // 2 to the power 3
+
+let str = "hello "
+let str1 = "Purab"
+
+let str2 = str + str1
+// console.log(str2);
+
+let counter = 100
+++counter;
+console.log(counter);
+
+
+
+
