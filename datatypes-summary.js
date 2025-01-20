8cf36c93c44fed1ca41b01cf01524d795eda4022
Purab <13purab@gmail.com>
2025-01-20 06:33:36 +0000

    datatype summary

---
+++ datatypes-summary.js
@@ -0,0 +1,40 @@
+// primitive
+
+// 7 types : String, Number, Boolean, null, undefined, symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail = undefined
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 1324343554656478n
+
+
+
+// Refernece type (Non - primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"]
+
+let myObj = {
+    name: "purab",
+    age: 23,
+}
+
+const myfunction= function(){
+    console.log("Hello world");
+    
+}
+
+console.log(typeof bigNumber);
+
+
+
