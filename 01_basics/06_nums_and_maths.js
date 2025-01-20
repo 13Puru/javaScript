04251cc85d0e014dd4f6066e83585d3125b5b2a5
Purab <13purab@gmail.com>
2025-01-20 18:52:59 +0000

    math and num

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,50 @@
+const score= 400
+// console.log(score);
+
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+
+const accountBal = 1134.556
+// console.log(accountBal.toPrecision(5));
+// console.log(accountBal.toPrecision(4));
+// // console.log(accountBal.toPrecision(3));
+
+
+const amount = 1000000000
+// console.log(amount.toLocaleString('en-IN')); //en-IN for Indian, and nothing for keeping it default US.
+
+
+
+// /////////////////////Maths///////////////////////
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.6));
+// console.log(Math.sqrt(25));
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max - min + 1)) + min);
+
+
+
+
+
+
+
+
+
+
+
+
+
