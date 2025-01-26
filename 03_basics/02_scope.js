4661010923232712d84e0d44ef96c35be2012720
Purab <13purab@gmail.com>
2025-01-26 12:55:19 +0000

    scope and hoisiting concept

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -1,6 +1,47 @@
 let a = 300
 if(true){
     let a = 30
-    console.log("inner: i.e. block scope: ", a); // inner value must not affect the global value so use let.
+    // console.log("inner: i.e. block scope: ", a); // inner value must not affect the global value so use let.
 }
-console.log("outer: i.e global scope: ", a);
+// console.log("outer: i.e global scope: ", a);
+
+
+// Nested
+function one(){
+    const username = "puru13"
+    function two(){
+        const web = "fb.com"
+        console.log(username);
+    }
+    // console.log(web);
+    // two()
+}
+// one()
+
+if (true) {
+    const user = "Purab"
+    if (user === "Purab") {
+        const website = " Github"
+        // console.log(user + website);
+    }
+    // console.log(website);
+}
+// console.log(user);
+
+
+// +++++++++++++++++++++++ Interesting ++++++++++++++++++++++++++++
+
+//hoisting concept
+console.log(addOne(5));  // function with below syntax can be hoisted before the actual lines of code.
+
+function addOne(num){
+    return num + 1
+}
+
+// ++++++++++++++++++++++++ concept 2 +++++++++++++++++++++++++++++
+console.log(addTwo(5)); // function with syntax below cant be hoisted before the actual lines of code.
+
+const addTwo = function(num){
+    return num + 2
+}
+ 
