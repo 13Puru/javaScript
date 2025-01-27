ffb681da500e1de1be13818a37ab904e42205fd8
Purab <13purab@gmail.com>
2025-01-27 15:38:27 +0000

    if, else, else if

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -8,14 +8,30 @@
 // }
 
 
-const score = 44
+// const score = 44
 
-if (score <= 100 && score > 70) {
-    let power = "can fly"
-    console.log(`Power is: ${power}`);
-}else if(score <= 70 && score >= 50){
-    let power = "can swim"
-    console.log(`power is: ${power}`);
-}else{
-    console.log(`power is: can walk`);
+// if (score <= 100 && score > 70) {
+//     let power = "can fly"
+//     console.log(`Power is: ${power}`);
+// }else if(score <= 70 && score >= 50){
+//     let power = "can swim"
+//     console.log(`power is: ${power}`);
+// }else{
+//     console.log(`power is: can walk`);
+// }
+
+const user = {
+    name: "Purab",
+    loggedInGoogle: false,
+    userLoggedIn: true,
+    loggedInEmail: true,
+    debitcard:true
+}
+
+if (user.userLoggedIn && user.debitcard) {
+    console.log(`${user.name}, logged in`);
+}
+
+if (user.loggedInEmail || user.loggedInGoogle) {
+    console.log(`${user.name}, logged in`);
 }
