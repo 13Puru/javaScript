e6146a048016b5d97a0482374f702dfc2ae7a706
Purab <13purab@gmail.com>
2025-01-28 18:57:29 +0000

    while, do while

---
+++ 05_itterations/two.js
@@ -0,0 +1,22 @@
+// while
+
+// let i = 0
+// while (i<=10) {
+//     console.log(`value of i is: ${i}`); 
+//     i = i + 1;
+// }
+
+// let arr = ["modi", `rahul`, `arvind`, `mamta`]
+// let j = 0
+// while (j < arr.length) {
+//     console.log(`the name of the politician is : ${arr[j]}`);
+//     j = j + 1
+// }
+
+
+// do while
+let score = 12
+do {
+    console.log(`score is ${score}`);
+    score = score + 1
+} while (score <= 10);
